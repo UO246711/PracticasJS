69fc238555c3238196a651ac457bf1b97f191d26
UO246711 <UO246711runiovi.es>
2020-11-30 13:30:13 +0100

    ejercicio1

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var informacion= new Object();
+informacion.asignatura="Software y estandares para la red";
+informacion.titulacion="Ingenieria informatica";
+informacion.centro="Escuela de ingenieria informatica";
+informacion.universidad="Universidad de oviedo";
+informacion.curso="Tercero";
+informacion.alumno="Enrique Vera";
+informacion.email="UO246711@uniovi.es"
---
+++ Parrafos.js
@@ -0,0 +1,14 @@
+document.write("<p>");
+document.write("Curso: ");
+document.write(informacion.curso);
+document.write("</p>\n");
+
+document.write("<p>");
+document.write("Alumno: ");
+document.write(informacion.alumno);
+document.write("</p>\n");
+
+document.write("<p>");
+document.write("Correo: ");
+document.write(informacion.email);
+document.write("</p>\n");
